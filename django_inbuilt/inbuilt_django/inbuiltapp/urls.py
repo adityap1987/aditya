90c825e08e5681368ab3cc8538383564a9340923
unknown <adityasharma.p@hotmail.com>
2022-02-20 19:43:30 +0530

    aditya369

---
+++ django_inbuilt/inbuilt_django/inbuiltapp/urls.py
@@ -0,0 +1,12 @@
+from inbuiltapp import views
+from django.urls import path
+
+app_name = 'inbuiltapp'
+
+urlpatterns = [
+   path('',views.index,name='index'),
+    path('register/',views.register,name='register'),
+    path("login/", views.user_login, name = "user_login"),
+    path("logout/", views.user_logout, name = "logout"),
+    path("special/", views.special, name = "special"),
+]
